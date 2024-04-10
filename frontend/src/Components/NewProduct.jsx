9dfb404af10bc23d91f24287f0dcef4ab9a5873e
Dogukan Kahraman <dkahraman08@gmail.com>
2024-04-09 17:19:13 -0700

    Some uI changes and remove from cart

--- frontend/src/Components/NewProduct.jsx
+++ frontend/src/Components/NewProduct.jsx
@@ -1,108 +1,120 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const NewProduct = () => {
-    const[name, setName] = useState('')
-    const[description, setDescription] = useState('')
-    const[price, setPrice] = useState('')
-    const[category, setCategory] = useState('')
-    const[categories, setCategories] = useState([]);
-    const[image, setImage] = useState({image : ""})
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [imageURL, setImageURL] = useState("");
 
-    useEffect(() => {
-        fetchCategories();
-    }, []);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
-    const fetchCategories = async () => {
-        try {
-            const response = await fetch('http://localhost:3000/categories');
-            const data = await response.json();
-            setCategories(data);
-        } catch (error) {
-            console.error('Error fetching categories:', error);
-        }
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('http://localhost:3000/products', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ 
-                    name: name,
-                    description: description,
-                    price: price,
-                    category: category,
-                    image: image
-                })
-            });
-            
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message || 'Failed to add product');
-            }
-            
-            window.location = '/';
-            console.log("Product Successfully Added")
-        } catch(error){
-            console.error(error);
-        }
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/categories");
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
     }
+  };
 
-    const handleFileUpload = async (e) => {
-        const file = e.target.files[0];
-        const base64 = await convertToBase64(file)
-        setImage(base64);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          description: description,
+          price: price,
+          category: category,
+          image: imageURL,
+        }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Failed to add product");
+      }
+
+      window.location = "/";
+      console.log("Product Successfully Added");
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    function convertToBase64(file) {
-        return new Promise((resolve, reject) => {
-            const fileReader = new FileReader()
-            fileReader.readAsDataURL(file);
-            fileReader.onload = () => {
-                resolve(fileReader.result)
-            }
-            fileReader.onerror = (error) => {
-                reject(error)
-            }
-        })
+  const handleCategory = (e) => {
+    const categoryName = e.target.value.trim();
+    const selectedCategory = categories.find(
+      (category) => category.name === categoryName
+    );
+    if (selectedCategory) {
+      setCategory(selectedCategory._id);
+    } else {
+      setCategory("");
     }
+  };
 
-    const handleCategory = (e) => {
-        const categoryName = e.target.value.trim();
-        const selectedCategory = categories.find(category => category.name === categoryName);
-        if (selectedCategory) {
-            setCategory(selectedCategory._id);
-        } else {
-            setCategory('');
-        }
-    };
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <div className="form-group">
+        <label htmlFor="name">Product Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
 
+        <label htmlFor="description">Product Description</label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          required
+          onChange={(e) => setDescription(e.target.value)}
+        />
 
-    return (
-<form onSubmit={handleSubmit} className="form-container">
-  <div className="form-group">
-    <label htmlFor="name">Product Name</label>
-    <input type="text" name="name" id="name" required onChange={(e) => setName(e.target.value)} />
+        <label htmlFor="price">Product Price</label>
+        <input
+          type="number"
+          name="price"
+          id="price"
+          required
+          onChange={(e) => setPrice(e.target.value)}
+        />
 
-    <label htmlFor="description">Product Description</label>
-    <input type="text" name="description" id="description" required onChange={(e) => setDescription(e.target.value)} />
+        <label htmlFor="category">Product Category (Case Sensitive)</label>
+        <input
+          type="text"
+          name="category"
+          id="category"
+          required
+          onChange={(e) => handleCategory(e)}
+        />
 
-    <label htmlFor="price">Product Price</label>
-    <input type="number" name="price" id="price" required onChange={(e) => setPrice(e.target.value)} />
+        <label htmlFor="image">Image Url</label>
+        <input
+          type="text"
+          name="image"
+          id="image"
+          required
+          onChange={(e) => setImageURL(e.target.value)}
+        />
+      </div>
 
-    <label htmlFor="category">Product Category (Case Sensitive)</label>
-    <input type="text" name="category" id="category" required onChange={(e) => handleCategory(e)} />
-
-    <label htmlFor="image">Upload new product image</label>
-    <input type="file" name="image" id="image" accept=".jpeg, .png, .jpg" required onChange={handleFileUpload} />
-  </div>
-
-  <button type="submit">Submit</button>
-</form>
-    )
-}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default NewProduct;
--- frontend/src/Components/ProductList.jsx
+++ frontend/src/Components/ProductList.jsx
@@ -1,39 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import Product from './Product';
+import React, { useState, useEffect } from "react";
+import Product from "./Product";
 
-const ProductList = ({ categoryName,cart_items,setItems,favourite,setFavourite,products }) => {  
-
+const ProductList = ({
+  categoryName,
+  cart_items,
+  setItems,
+  favourite,
+  setFavourite,
+  products,
+}) => {
   const handleFavClick = (product) => {
-    const index = favourite.findIndex(likedProduct => likedProduct._id === product._id);
+    const index = favourite.findIndex(
+      (likedProduct) => likedProduct._id === product._id
+    );
     if (index !== -1) {
-        // If the product is already liked, remove it from the liked products list
-        const updatedLikedProducts = [...favourite];
-        updatedLikedProducts.splice(index, 1);
-        setFavourite(updatedLikedProducts);
-      
-
+      // If the product is already liked, remove it from the liked products list
+      const updatedLikedProducts = [...favourite];
+      updatedLikedProducts.splice(index, 1);
+      setFavourite(updatedLikedProducts);
     } else {
-        // If the product is not liked, add it to the liked products list
-        setFavourite([...favourite, product]);
+      // If the product is not liked, add it to the liked products list
+      setFavourite([...favourite, product]);
     }
-}
-const handleItems = (product) => {
-    const index = cart_items.findIndex(item => item._id === product._id);
+  };
+  const handleItems = (product) => {
+    const index = cart_items.findIndex((item) => item._id === product._id);
     if (index !== -1) {
-        const updatedItems = [...cart_items];
-        updatedItems[index].quantity += 1;
-        setItems(updatedItems);
+      const updatedItems = [...cart_items];
+      updatedItems[index].quantity += 1;
+      setItems(updatedItems);
     } else {
-        setItems([...cart_items, { ...product, quantity: 1 }]);
+      setItems([...cart_items, { ...product, quantity: 1 }]);
     }
-};
-
+  };
 
   return (
-    <div className='container w-full mx-auto flex gap-5 flex-wrap mt-5 pl-7 z-20'>
-      
+    <div className="container mx-auto mt-5 flex gap-5 flex-wrap px-10 z-20">
       {products
-        .filter(product => !categoryName || product.category === categoryName) // Filter products by category name
+        .filter((product) => !categoryName || product.category === categoryName) // Filter products by category name
         .map((product) => (
           <Product
             key={product._id}
@@ -44,9 +48,7 @@ const handleItems = (product) => {
             click={() => handleItems(product)}
             favClick={() => handleFavClick(product)}
           />
-        ))
-
-        }
+        ))}
     </div>
   );
 };
